241139701e9eddb85ff34a229db382c4b206a50c
m152196 <m152196@usna.edu>
2014-09-30 09:48:40 -0400

    Finalized partG

--- partG/proj.rb
+++ partG/proj.rb
@@ -9,12 +9,10 @@ class Integer # The bottles
   # create an Instance method that lowers number of bottles by 1
   def drink
     #$increment = ( rand(10) + 1)
-    #self + $increment
     
     # Only allow a random number that does not result in the specified end point being exceeded
     self + ($remaining < 10 ? rand($remaining) + 1 : rand(10) + 1)
   end
-  #def drink; self - 1; end
 end
 
 # Creates an instance of Object, named 'song', and opens up a singleton class.
@@ -44,13 +42,11 @@ class << song = nil
     #   the number of bottles that is initially specified in the block of the 'callcc' command.
     #  	This class, with the new method, is returned.
     (class << self; self; end).module_eval do
-      #define_method(:buy) { bottles }
       define_method(:buy) { bottles * 110}
     end
     self
   end
 
-  
   def sing(&step)# '&step', the block { |beer| beer.drink }, is converted to a Proc object, which is assigned to the parameter.
     #    Used this way, the value acessible insude 'sing()' when 'step' is referenced is a number (<= initial number
     #	  specified). However, when 'step' == 0, 'step = method :buy' gets executed, changing 'step' into the method ':buy'.
@@ -65,14 +61,6 @@ class << song = nil
     #if @bottles.zero?
     tmp = @bottles
     @bottles = step[@bottles]
-    #puts ""
-    #puts "tmp = #{tmp}"
-    #puts "@bottles = #{@bottles}"
-    #puts "step[tmp] = #{step[tmp]}"
-    #puts "step[@bottles] = #{step[@bottles]}"
-    #puts "step[@bottles] = #{step[@bottles]}"
-    #puts "step[@bottles] = #{step[@bottles]}"
-    #puts ""
     if @bottles >= $total_iterations #100
       #print "Go to the store buy some more, "
       print "Print #{@bottles - tmp} out, stand up and shout, "
@@ -83,7 +71,7 @@ class << song = nil
       print "Print #{@bottles - tmp} out, stand up and shout, "
     end
     #@bottles = step[@bottles] # Equivalent to step.call(@bottles) --> jumps back up to the 'sing()' method with @bottles as the argument
-    puts "#{prime_facts(bottles)} of text on the screen."
+    puts "#{prime_fact(bottles)} of text on the screen."
     # Pause for 1 second after printing out each stanza, for "dramatic effect"
     sleep(1)
     
@@ -94,37 +82,32 @@ class << song = nil
   end
 end
 
-  def prime_fact(num)
-    facts = []
-	while num % 2 == 0 do
+def prime_fact(num)
+  num = num.to_i
+  facts = []
+    while num % 2 == 0 do
       facts << 2
       num = num./(2)
-	end
+    end
 
-	lower = 3
-	while lower <= num/2 do
+    lower = 3
+    while lower <= ( num / 2 ) do
       while num % lower == 0 do
         facts << lower
         num = num./(lower)
-	  end
+      end
       lower += 2
-	end
+    end
     if num != 0 and num != 1
       facts << num
     end
-  '(' + facts.join('*') + ')'
-  #facts
+  ' (' + facts.join('*') + ')'
 end
-
-
-  
 
   print "How many lines? "
   STDOUT.flush
   $total_iterations = gets.chomp.to_i
   $remaining = $total_iterations
-  puts "User entered #{$total_iterations}"
-  puts "The factors of #{$total_iterations} are #{prime_fact($total_iterations)}"
-  # Call the Bottles of Beer song, starting with 15 bottles on the wall
-  #callcc { |song.wall| song.of(1) }.sing { |beer| beer.drink }
+  # Call the Bottles of Beer song, starting with 1 bottles on the wall
+  callcc { |song.wall| song.of(1) }.sing { |beer| beer.drink }
 
